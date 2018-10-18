3d68a3e6d92bf596a29ccd2dc8b4dfdd41b79266
Justin <nagasnow@Justins-iMac.local>
2018-10-17 21:07:30 -0700

    added tables for tasks

---
+++ TodoList/TodoList/TodoListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TodoListViewController.swift
+//  TodoList
+//
+//  Created by Justin on 10/17/18.
+//  Copyright © 2018 Justin. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var todoItems: [String] = ["Brush Teeth", "Do Homework", "Make Dinner"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let todoItem = todoItems[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell")!
+        cell.textLabel?.text = todoItem
+        return cell
+    }
+}
